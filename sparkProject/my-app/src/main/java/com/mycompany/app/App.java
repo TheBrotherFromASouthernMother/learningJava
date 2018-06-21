02af1088db260b0bb0657fa9027dfbac2e6c25eb
Christian Lowe <christglowe@gmail.com>
2018-06-21 13:38:08 -0600

    prepared java app for deployment

--- sparkProject/my-app/src/main/java/com/mycompany/app/App.java
+++ sparkProject/my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,11 @@ public class App
 {
     public static void main( String[] args )
     {
+      try {
+           Spark.port(Integer.valueOf(System.getenv("PORT"))); // needed for Heroku
+         } catch (Exception e) {
+             System.err.println("NOTICE: using default port.  Define PORT env variable to override");
+         }
         System.out.println( "Hello World!" );
         get("/hello", (req, res) -> "Hello World");
         get("/", (req, res) -> "NOOOOOO");
