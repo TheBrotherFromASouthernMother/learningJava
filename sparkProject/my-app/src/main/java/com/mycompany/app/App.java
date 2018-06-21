44fa00c0cd9fcfb232358d0fd0a3d924cfdf5ddd
Christian Lowe <christglowe@gmail.com>
2018-06-21 13:57:34 -0600

    moved a bunch of progam files to another folder so as to make the top directory less cluttered

--- sparkProject/my-app/src/main/java/com/mycompany/app/App.java
+++ sparkProject/my-app/src/main/java/com/mycompany/app/App.java
@@ -9,11 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-      try {
-           Spark.port(Integer.valueOf(System.getenv("PORT"))); // needed for Heroku
-         } catch (Exception e) {
-             System.err.println("NOTICE: using default port.  Define PORT env variable to override");
-         }
+        setPort(Integer.valueOf(System.getenv("PORT")));
         System.out.println( "Hello World!" );
         get("/hello", (req, res) -> "Hello World");
         get("/", (req, res) -> "NOOOOOO");
