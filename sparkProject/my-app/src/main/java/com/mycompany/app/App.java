cda19dea5e985da3a2e66a2c631e4077064e81ce
Christian Lowe <christglowe@gmail.com>
2018-06-21 14:38:06 -0600

    testing deployment otra vez

--- sparkProject/my-app/src/main/java/com/mycompany/app/App.java
+++ sparkProject/my-app/src/main/java/com/mycompany/app/App.java
@@ -5,13 +5,20 @@ import static spark.Spark.*;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        setPort(Integer.valueOf(System.getenv("PORT")));
-        System.out.println( "Hello World!" );
-        get("/hello", (req, res) -> "Hello World");
-        get("/", (req, res) -> "NOOOOOO");
+
+public class App {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
